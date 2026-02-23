9feb46f800a25b0a10d27112d43d04a46a153534
Felipe Elias <felipedaniel.me@gmail.com>
2026-02-23 13:08:04 -0300

    style: set sidebar position as sticky

--- src/components/SideBar/styles.ts
+++ src/components/SideBar/styles.ts
@@ -5,5 +5,8 @@ export const StyledSideBar = styled.aside`
     display: flex;
     flex-direction: column;
     gap: ${theme.space[4]};
+    position: sticky;
+    top: calc(${theme.space[8]} + ${theme.defaults.headerHeight});
+    left: 0;
   `}
 `
